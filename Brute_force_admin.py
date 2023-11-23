c1efb590b73f6be8c6d8fac45991711795262bf1
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-11-23 15:06:06 +0100

    Update Brute_force_admin.py

--- Brute_force_admin.py
+++ Brute_force_admin.py
@@ -4,14 +4,15 @@ from scapy.all import *
 def generate_passwords(characters, length):
     return [''.join(p) for p in itertools.product(characters, repeat=length)]
 
-def try_credentials(username, password, target_ip):
-    # W tym miejscu należy dodać logikę próby uwierzytelnienia na urządzeniu sieciowym.
+def try_credentials(username, password, target_ip, target_port):
+    # W tym miejscu można dodać logikę próby uwierzytelnienia na urządzeniu sieciowym.
     # Na potrzeby przykładu, używamy funkcji print, co jest niebezpieczne i niezalecane w rzeczywistych testach.
 
-    print(f"Trying {username}:{password} on {target_ip}")
+    print(f"Trying {username}:{password} on {target_ip}:{target_port}")
 
 def main():
-    target_ip = "192.168.1.1"
+    target_ip = input("Podaj IP: ")
+    target_port = input("Podaj Port: ")
     username = "admin"
     character_set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     password_length = 8
@@ -19,7 +20,7 @@ def main():
     passwords = generate_passwords(character_set, password_length)
 
     for password in passwords:
-        try_credentials(username, password, target_ip)
+        try_credentials(username, password, target_ip, target_port)
 
 if __name__ == "__main__":
     main()
